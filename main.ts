84d0e57ff679fe3887a6e1605addbd28dbdbab21
crvillarroel <crvillarroel@hotmail.com>
2025-12-12 21:50:47 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -14,22 +14,27 @@ scene.cameraFollowSprite(myPlayer)
 tiles.setCurrentTilemap(tilemap`level3`)
 if (Math.percentChance(0.01)) {
     myEnemyBacteria = sprites.create(img`
-        . . . . . . 2 2 2 2 . . . . . . 
-        . . . . 2 2 3 3 3 3 2 e . . . . 
-        . . . 2 3 d 1 1 d d 3 2 e . . . 
-        . . 2 3 1 d 3 3 3 d d 3 e . . . 
-        . 2 3 1 3 3 3 3 3 d 1 3 b e . . 
-        . 2 1 d 3 3 3 3 d 3 3 1 3 b b . 
-        2 3 1 d 3 3 1 1 3 3 3 1 3 4 b b 
-        2 d 3 3 d 1 3 1 3 3 3 1 3 4 4 b 
-        2 d 3 3 3 1 3 1 3 3 3 1 b 4 4 e 
-        2 d 3 3 3 1 1 3 3 3 3 1 b 4 4 e 
-        e d 3 3 3 3 d 3 3 3 d d b 4 4 e 
-        e d d 3 3 3 d 3 3 3 1 3 b 4 b e 
-        e 3 d 3 3 1 d d 3 d 1 b b e e . 
-        . e 3 1 1 d d 1 1 1 b b e e e . 
-        . . e 3 3 3 3 3 3 b e e e e . . 
-        . . . e e e e e e e e e e . . . 
+        . . . . f f f f . . . . 
+        . . f f f f f f f f . . 
+        . f f f f f f f f f f . 
+        f f f f f f f f f f f f 
+        f f f f f f f f f f f f 
+        f f f f f f f f f f f f 
+        f f f f f f f f f f f f 
+        f f f f f f f f f f f f 
+        f f f f f f f f f f f f 
+        . f f f f f f f f f f . 
+        . f f f f f f f f f f . 
+        f f f f f f f f f f f f 
+        . . f . . f f . . f . . 
+        . . f . . . f . f f . . 
+        . f f . . f f . f . . . 
+        . f f . . f f . f . . . 
+        . . f . . f . . f f . . 
+        . f f . f . f f . f f . 
+        . f . . f . . f . . f . 
+        f f . f f . . f . . f f 
+        . f . f . . . f f . f . 
         `, SpriteKind.Enemy)
     tiles.placeOnRandomTile(myEnemyBacteria, assets.tile`floor`)
     myEnemyBacteria.setVelocity(45, 45)
