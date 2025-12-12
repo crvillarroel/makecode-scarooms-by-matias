45d5e585fc0ed46819345ef6aacba1dfc993e046
crvillarroel <crvillarroel@hotmail.com>
2025-12-12 21:33:24 +0100

    myEnemyBacteria is ready

--- main.ts
+++ main.ts
@@ -1,9 +1,37 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 	
 })
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    game.gameOver(false)
+})
 function feed (mySprite: Sprite) {
 	
 }
+let myEnemyBacteria: Sprite = null
 let myPlayer = sprites.create(assets.image`Player`, SpriteKind.Player)
 controller.moveSprite(myPlayer)
 scene.cameraFollowSprite(myPlayer)
+tiles.setCurrentTilemap(tilemap`level3`)
+if (Math.percentChance(0.01)) {
+    myEnemyBacteria = sprites.create(img`
+        . . . . . . 2 2 2 2 . . . . . . 
+        . . . . 2 2 3 3 3 3 2 e . . . . 
+        . . . 2 3 d 1 1 d d 3 2 e . . . 
+        . . 2 3 1 d 3 3 3 d d 3 e . . . 
+        . 2 3 1 3 3 3 3 3 d 1 3 b e . . 
+        . 2 1 d 3 3 3 3 d 3 3 1 3 b b . 
+        2 3 1 d 3 3 1 1 3 3 3 1 3 4 b b 
+        2 d 3 3 d 1 3 1 3 3 3 1 3 4 4 b 
+        2 d 3 3 3 1 3 1 3 3 3 1 b 4 4 e 
+        2 d 3 3 3 1 1 3 3 3 3 1 b 4 4 e 
+        e d 3 3 3 3 d 3 3 3 d d b 4 4 e 
+        e d d 3 3 3 d 3 3 3 1 3 b 4 b e 
+        e 3 d 3 3 1 d d 3 d 1 b b e e . 
+        . e 3 1 1 d d 1 1 1 b b e e e . 
+        . . e 3 3 3 3 3 3 b e e e e . . 
+        . . . e e e e e e e e e e . . . 
+        `, SpriteKind.Enemy)
+    tiles.placeOnRandomTile(myEnemyBacteria, assets.tile`floor`)
+    myEnemyBacteria.setVelocity(45, 45)
+    myEnemyBacteria.follow(myPlayer)
+}
